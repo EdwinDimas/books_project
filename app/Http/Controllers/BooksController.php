9f991bd9b4906ea2aa88d4ed254c2710538ea5fe
EdwinDimas <edwinenriquedimas@gmail.com>
2023-05-04 14:10:39 -0600

    Base structure for POST, GET, DELETE books

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+
+class BooksController extends Controller
+{
+
+
+    public function store(Request $request){
+        $book = new Book;
+        $book->title = $request->input('title');
+        $book->author = $request->input('author');
+        $book->description = $request->input('description');
+        $book->save();
+        return response()->json(['message' => 'Book created', 'book' => $book], 201);
+    }
+
+    public function index(){
+        $books = Book::all();
+        return response()->json(['books' => $books], 200);
+    }
+
+    public function destroy($book_id){
+        $book = Book::find($book_id);
+        if ($book) {
+            $book->delete();
+            return response()->json(['message' => 'Book deleted'], 200);
+        } else {
+            return response()->json(['error' => 'Book not found'], 404);
+        }
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
+Route::post('/books', 'BooksController@store');
+Route::get('/books', 'BooksController@index');
+Route::delete('/books/{book}', 'BooksController@destroy');
+
+
