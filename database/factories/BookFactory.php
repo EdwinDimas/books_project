346ff73cbd16d2cd82a8b1bdb30885214f19c532
Edwin Dimas <edwinenriquedimas@gmail.com>
2023-05-05 22:32:57 -0600

    Making fake testing Books data with factories and seeders

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Book;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class BookFactory extends Factory
+{
+
+
+    protected $model = Book::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence,
+            'author' => fake()->name,
+            'description' => fake()->sentence,
+            'user_id' => fake()->randomElement(['1','2']),
+        ];
+    }
+}
